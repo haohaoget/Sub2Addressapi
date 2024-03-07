ff72b51da7b1934df6a28039a39a335a3f0b9dcb
maxocean <39007967+haohaoget@users.noreply.github.com>
2024-03-07 19:04:25 +0800

    Update _worker.js 定时+kv存储-性能+抽奖

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 
 let sub = ['cm.git.cloudns.biz'];
-const hostName = 'test.xyz';
+const hostName = 'cm.git.xyz';
 const userID = '90cd4a77-141a-43c9-991b-08263cfe9c10';
 const ipSet = new Set();
 
@@ -16,11 +16,12 @@ function randomSample(arr, n) {
 
 function extractInfo(encodedUrls) {
   let lines = encodedUrls.split('\n');
-  const extractedData = [];
+  let extractedData = [];
 
   const nameCountMap = new Map();
-  //抽奖
-  lines = lines.length > 20 ? randomSample(lines, 20) : lines;
+  //console.log("lines content:", lines);
+  //提高性能抽奖
+  //lines = lines.length > 20 ? randomSample(lines, 20) : lines;
 
   for (const line of lines) {
     const match = line.match(/@([\d.]+):(\d+)\?/);
@@ -30,9 +31,9 @@ function extractInfo(encodedUrls) {
       const name = hashIndex !== -1 ? line.slice(hashIndex + 1) : '';
 
       // 包含中文字符(一般有广告)跳过
-      if (/[\u4e00-\u9fa5]/.test(name)) {
-        continue;
-      }
+      // if (/[\u4e00-\u9fa5]/.test(name)) {
+      //   continue;
+      // }
 
       // 去掉重复的 IP 地址
       if (ipSet.has(ipAddress)) {
@@ -49,52 +50,76 @@ function extractInfo(encodedUrls) {
         nameCountMap.set(name, count + 1);
       }
 
-      // 总数不超过12个(自取前12个)
-      if (nameCountMap.size >= 12) {
-        break;
-      }
+      // 配合高性能抽奖-总数不超过12个(自取前12个)
+      // if (nameCountMap.size >= 12) {
+      //   break;
+      // }
     }
   }
-
+  
+  //低性能抽奖-总数不超过12个(自取前12个)
+  extractedData = extractedData.length > 20 ? randomSample(extractedData, 6) : randomSample(extractedData, 8);
+  
   return extractedData;
 }
 
-export default {
-	async fetch(request, env) {
-    const url = new URL(request.url);
-    sub = env.SUB ? env.SUB.split(",") : sub;
-    const subQueryParam = url.searchParams.get('sub');
-    sub = subQueryParam ? subQueryParam.split(",") : sub;
-    
-    let addressapi = [];
-    ipSet.clear();
-		try {
-      await Promise.all(sub.map(async (subs) => {
-        const response = await fetch(`https://${subs}/sub?host=${hostName}&uuid=${userID}&path=?ed2048&edgetunnel=cmliu`);
-        if (response.ok) {
-          const content = await response.text();
-          if (content.length && content.length % 4 === 0 && !/[^A-Z0-9+\/=]/i.test(content)) {
-            //console.log("content:", content);
-            const decodedContent = decodeURI(atob(content)); // Base64 decoding
-            //console.log("Decoded content:", decodedContent);
-            const newaddressapi = extractInfo(decodedContent);
-            addressapi = addressapi.concat(newaddressapi);
-          }
+async function getapi(env) {
+  const SUB_BUCKET = env.SUB_BUCKET || false;
+  
+  ipSet.clear();
+  let addressapi = [];
+  try {
+    //await Promise.all(sub.map(async (subs) => {
+    for (const subs of sub){
+      const response = await fetch(`https://${subs}/sub?host=${hostName}&uuid=${userID}&path=?ed2048&edgetunnel=cmliu`);
+      //console.log("subs content:", subs);
+      if (response.ok) {
+        const content = await response.text();
+        if (content.length && content.length % 4 === 0 && !/[^A-Z0-9+\/=]/i.test(content)) {
+          //console.log("content:", content);
+          const decodedContent = decodeURI(atob(content)); // Base64 decoding
+          //console.log("Decoded content:", decodedContent);
+          const newaddressapi = extractInfo(decodedContent);
+          //console.log("newaddressapi content:", newaddressapi);
+          addressapi = addressapi.concat(newaddressapi);
         }
-      }));
+      }
+    }
+    //}));
 
-      if (addressapi.length >= 0){
-        const Raddressapi = addressapi.join("\n");
-        return new Response(Raddressapi, {
-          status: 200,
-          headers: { 'content-type': 'text/plain; charset=utf-8' },
-        });
+    if (addressapi.length >= 0){
+      const Raddressapi = addressapi.join("\n");
+      if(SUB_BUCKET){
+        await SUB_BUCKET.put('addressapi', Raddressapi);
       }
       
-		} catch (err) {
-			/** @type {Error} */ 
-      let e = err;
-			return new Response(e.toString());
-		}
-	},
+      return new Response(Raddressapi, {
+        status: 200,
+        headers: { 'content-type': 'text/plain; charset=utf-8' },
+      });
+    }
+  
+    
+  } catch (err) {
+    /** @type {Error} */ 
+    let e = err;
+    return new Response(e.toString());
+  }
+}
+
+export default {
+  async scheduled(event, env){
+    sub = env.SUB ? env.SUB.split(",") : sub;
+    return getapi(env);
+  },
+
+  async fetch(request, env) {
+    sub = env.SUB ? env.SUB.split(",") : sub;
+    const url = new URL(request.url);
+    const subQueryParam = url.searchParams.get('sub');
+    sub = subQueryParam ? subQueryParam.split(",") : sub;
+
+    return getapi(env);
+		
+  },
 };
